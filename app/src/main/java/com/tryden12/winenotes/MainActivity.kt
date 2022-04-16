4c393b0de36b48b5b62dbaea67f6ccea6e5b35c2
Tyler Ryden <tryden11@gmail.com>
2022-04-16 11:39:03 -0500

    added bg image

--- app/src/main/java/com/tryden12/winenotes/MainActivity.kt
+++ app/src/main/java/com/tryden12/winenotes/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.tryden12.winenotes
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.tryden12.winenotes.databinding.ActivityMainBinding
@@ -12,5 +13,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
     }
 }
