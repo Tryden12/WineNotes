bc669b3b0e1478185a8818b93b318dd975f34970
Tyler Ryden <tryden11@gmail.com>
2022-04-18 20:45:06 -0500

    if purpose is update

--- app/src/main/java/com/tryden12/winenotes/database/NoteDao.kt
+++ app/src/main/java/com/tryden12/winenotes/database/NoteDao.kt
@@ -24,5 +24,6 @@ interface NoteDao {
     @Query("SELECT * FROM note")
     fun getAllNotes() : List<Note>
 
-
+    @Query("SELECT * FROM note WHERE id = :noteId")
+    fun getNote(noteId : Long) : Note
 }
